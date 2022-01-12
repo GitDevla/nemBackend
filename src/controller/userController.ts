106e4882bab6e15a06ed97f2883751fb2eeaccd5
GitDevla <pataki.david.ferenc@gmail.com>
2022-01-12 10:24:32 +0100

    regisztáció tokent is visszaküldi

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { generateToken } from '../service/authService';
 import { createUser, findUser } from '../service/userService';
 import { Conflict } from '../util/ApiErrors';
 import responseWrapper from '../util/responseWrapper';
@@ -8,5 +9,8 @@ export const registerHandler = async (req: Request<{}, {}, registerType>, res: R
 	const existingUser = await findUser(req.body.email);
 	if (existingUser) throw new Conflict('Ez a felhasználó már létezik');
 	const createdUser = await createUser(req.body);
+	const token = await generateToken(createdUser);
+
+	responseWrapper(res, token);
 	responseWrapper(res);
 };
